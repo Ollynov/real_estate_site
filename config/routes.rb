a37171bafa3d5c231fd3d0dc688c5baf1e0bb464
Aljosha Novakovic <ollynov@gmail.com>
2017-03-26 22:29:41 -0700

    Added css styling to buttons and profile page

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
              :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
              :controllers => {:omniauth_callbacks => 'omniauth_callbacks'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show]
 end
